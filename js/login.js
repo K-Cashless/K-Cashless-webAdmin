390d51ef5a77ce5118fa8cbc32b59050c36baabb
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2020-04-15 23:20:21 +0700

    import axios and POST to api

--- js/login.js
+++ js/login.js
@@ -1,11 +1,9 @@
 function loginFunc() {
-    //import axios from 'axios';
     document.getElementById("info").innerHTML = "YOU CLICKED ME!";
-    /*
+
     var info = {email:document.getElementById("email").value,password:document.getElementById("pass").value};
     console.log(info);
     axios.post('https://asia-east2-k-cash-less.cloudfunctions.net/api/login',info)
-    .catch(error => alert(error));
-    return false;
-    */
+        .then(value => alert('SUCCESS'))
+        .catch(error => alert(error));
   }
