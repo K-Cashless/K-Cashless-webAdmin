02925f0f24d465e89aea0003d2062ba4a14688d0
Kittinat A <39628446+porsmart@users.noreply.github.com>
2020-04-18 23:55:20 +0700

    add login and test table api
    add test.css
    add test.html
    add test.js //

--- js/login.js
+++ js/login.js
@@ -4,6 +4,9 @@ function loginFunc() {
     var info = {email:document.getElementById("email").value,password:document.getElementById("pass").value};
     console.log(info);
     axios.post('https://asia-east2-k-cash-less.cloudfunctions.net/api/login',info)
-        .then(value => alert('SUCCESS'))
+        .then(value => {
+            alert('SUCCESS')
+            window.location.replace("main.html");
+        })
         .catch(error => alert(error));
   }
